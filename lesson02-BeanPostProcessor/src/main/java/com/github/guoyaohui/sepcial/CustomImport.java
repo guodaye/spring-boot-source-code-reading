1a4552770c4897eb89b4b46630e796110de2ef5a
郭垚辉 <gyh657614602@126.com>
2018-08-16 21:30:57 +0800

    添加了关键的步骤

--- lesson02-BeanPostProcessor/src/main/java/com/github/guoyaohui/sepcial/CustomImport.java
+++ lesson02-BeanPostProcessor/src/main/java/com/github/guoyaohui/sepcial/CustomImport.java
@@ -1,15 +1,15 @@
 package com.github.guoyaohui.sepcial;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 /**
  * @author 郭垚辉
  * @date 2018/08/16
  */
 @Import(HelloWorld.class)
-@Component
+@Configuration
 public class CustomImport {
 
     @Bean
---
+++ lesson02-BeanPostProcessor/src/main/java/com/github/guoyaohui/sepcial/CustomProperties.java
@@ -0,0 +1,12 @@
+package com.github.guoyaohui.sepcial;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author 郭垚辉
+ * @date 2018/08/16
+ */
+@ConfigurationProperties(value = "file")
+public class CustomProperties {
+
+}
